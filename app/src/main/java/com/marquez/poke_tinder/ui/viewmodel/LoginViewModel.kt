b74b3815e9b3927b1a46f7762196c1aab9d1d5f4
SebastianMP21 <sebastian.marquez@tecsup.edu.pe>
2022-05-16 11:49:59 -0500

    Lab08_09 v1.0

---
+++ app/src/main/java/com/marquez/poke_tinder/ui/viewmodel/LoginViewModel.kt
@@ -0,0 +1,35 @@
+package com.marquez.poke_tinder.ui.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.marquez.poke_tinder.data.User
+import com.marquez.poke_tinder.util.SharedPreferenceUtil
+
+class LoginViewModel(private val context: Context): ViewModel(){
+    private lateinit var sharedPreferenceUtil: SharedPreferenceUtil
+
+    val emptyFieldsError = MutableLiveData<Boolean>()
+    val fieldsAuthenticateError = MutableLiveData<Boolean>()
+    val goSuccesActivity = MutableLiveData<Boolean>()
+
+    fun onCreate() {
+        sharedPreferenceUtil = SharedPreferenceUtil().also {
+            it.setSharedPreference(context)
+        }
+    }
+
+    fun validateInputs(email:String, password: String) {
+        if (email.isEmpty() && password. isEmpty()) {
+            emptyFieldsError.postValue(true)
+        }else {
+            val user: User? = sharedPreferenceUtil.getUser()
+
+            if (email.equals(user?.email) && password.equals(user?.password)) {
+                goSuccesActivity.postValue(true)
+            } else {
+                fieldsAuthenticateError.postValue(true)
+            }
+        }
+    }
+}
