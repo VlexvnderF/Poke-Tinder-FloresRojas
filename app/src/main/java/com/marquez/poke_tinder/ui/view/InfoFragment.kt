16bc64dd010e261b57029fc8a64a5d0d3e3c82a4
SebastianMP21 <sebastian.marquez@tecsup.edu.pe>
2022-06-03 23:33:10 -0500

    Lab11

---
+++ app/src/main/java/com/marquez/poke_tinder/ui/view/InfoFragment.kt
@@ -0,0 +1,49 @@
+package com.marquez.poke_tinder.ui.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.marquez.poke_tinder.R
+import com.marquez.poke_tinder.databinding.FragmentInfoBinding
+import com.marquez.poke_tinder.ui.viewmodel.InfoViewModel
+
+class InfoFragment : BaseFragment<FragmentInfoBinding>(FragmentInfoBinding::inflate) {
+
+    private lateinit var viewModel: InfoViewModel
+
+    private lateinit var webView: WebView
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        webView.saveState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val url = "https://pokemongolive.com/es/"
+
+        webView = binding.wvMain
+
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = PokemonWebClient()
+        savedInstanceState?.let {
+            webView.restoreState(it)
+        }?:webView.loadUrl(url)
+    }
+
+    inner class PokemonWebClient: WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            webView.loadUrl("javascript:(function() { " +
+                    "document.getElementByClassName('navbar top')[0].style.display='none'; })()")
+        }
+    }
+
+}
---
+++ app/src/main/java/com/marquez/poke_tinder/ui/viewmodel/InfoViewModel.kt
@@ -0,0 +1,7 @@
+package com.marquez.poke_tinder.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class InfoViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
